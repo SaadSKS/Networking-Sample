2aea4bb6e314ec087bd7c32f0b23441516e9fcbc
SaadSKS <saad.sks@gmail.com>
2019-06-16 00:29:47 +0530

    Successful Demo Scene
    LAN Test PASSED
    Online Direct IP Connection Test PASSED
    (Via Ignorance Transport)

---
+++ Networking Sample/Assets/Demo/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Mirror
+{
+    public class Movement : NetworkBehaviour
+    {
+
+        public float speed = 30;
+
+        // need to use FixedUpdate for rigidbody
+        void FixedUpdate()
+        {
+            // only let the local player control the racket.
+            // don't control other player's rackets
+            if (!isLocalPlayer) return;
+
+            float vertical = Input.GetAxisRaw("Vertical");
+            float h = Input.GetAxisRaw("Horizontal");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(h, vertical) * speed * Time.fixedDeltaTime;
+        }
+    }
+}
